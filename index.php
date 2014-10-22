cb96ccd3ab3ba013498ca9f2342cbaba17063754
liuin <20893791@qq.com>
2014-10-22 23:28:35 +0800

    update

--- index.php
+++ index.php
@@ -4,12 +4,18 @@
     padding:1em;
     border:1px solid #000;
   }
+  .active {
+    background:red;
+  }
 </style>
 <div class="container">
   <div class="row">
     <div class="col-md-9">
       <div class="alert">
         <button id="btn-alert" data-dismiss="alert"><span>button</span></button>
+        <button type="button" class="btn btn-primary" data-toggle="button" autocomplete="off">
+          Single toggle
+        </button>
         
       </div>
 
